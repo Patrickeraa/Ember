6864c0ce2e8211e56c7ad4a9a78ff94318837576
Patrickeraa <patrick.araujo@ufv.br>
2026-04-03 20:07:15 -0300

    test

--- sendall.sh
+++
@@ -1,6 +0,0 @@
-docker exec grworker1 rm -rf /workspace/files
-docker exec grworker2 rm -rf /workspace/files
-docker exec grserver rm -rf /workspace/files
-docker cp /home/patrick.araujo/GRPC/worker/files grworker1:/workspace
-docker cp /home/patrick.araujo/GRPC/worker/files grworker2:/workspace
-docker cp /home/patrick.araujo/GRPC/server/files grserver:/workspace
